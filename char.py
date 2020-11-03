063584c991752cb0b0a22ab6098384889ac862ea
Nathan Higley <nathan@nathanhigley.com>
2020-11-02 22:15:17 -0500

    Added initial files

---
+++ char.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+char.py
+Nathan Higley <nathan@nathanhigley.com>
+Nov 2, 2020
+
+Small python program to convert ascii codes to chars and vice versa.
+"""
+
+import argparse
+
+desc = "Converts ascii codes to chars and vice versa"
+
+parser = argparse.ArgumentParser(description=desc)
+
+parser.parse_args()
+
+
